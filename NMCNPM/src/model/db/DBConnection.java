1e40d21a9701c80cb1d1ac7d4d4badcdcc2ad91c
Chauptm <71380488+Chauptm@users.noreply.github.com>
2020-11-23 21:54:51 +0700

    Update

--- NMCNPM/src/model/db/DBConnection.java
+++ NMCNPM/src/model/db/DBConnection.java
@@ -1,5 +1,20 @@
 package model.db;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
+public class DBConnection {
+	public static Connection getConnection() {
+		Connection conn= null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String url = "jdbc:sqlserver://ADMINISTRATOR\\SQLEXPRESS:1433;databaseName=dbCNPM;";
+			conn = DriverManager.getConnection(url,"sa", "123456789");
+			System.out.println("okay");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+		}
+		return conn;
+	}
 }
