ee41a3095ec0ef4d8f83f3944744218d2e9e390e
vân <01658635754levan@gmail.com>
2020-11-24 09:21:29 +0700

    add notice

--- NMCNPM/src/model/db/DBConnection.java
+++ NMCNPM/src/model/db/DBConnection.java
@@ -8,9 +8,9 @@ public class DBConnection {
 	public static Connection getConnection() {
 		Connection conn= null;
 		try {
-			// Đổi 'DESKTOP-77O14EV' thành tên máy local 
+			// Change 'DESKTOP-77O14EV' to your local destop's name
 			String url = "jdbc:sqlserver://DESKTOP-77O14EV\\SQLEXPRESS:1433;databaseName=dbCNPM;";
-			conn = DriverManager.getConnection(url,"sa", "12345678"); // đổi mk 
+			conn = DriverManager.getConnection(url,"sa", "12345678"); // change the password 
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
 		}
