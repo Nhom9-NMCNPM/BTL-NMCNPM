176af481343aef18b065247d75ac1ec75bf24237
= <=>
2020-11-24 09:16:35 +0700

    them lop AdminDAO

---
+++ NMCNPM/src/model/businesslogic/AdminDAO.java
@@ -0,0 +1,35 @@
+package model.businesslogic;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import model.bean.SuKien;
+
+public class AdminDAO {
+	
+	public ArrayList<SuKien> getSuKien(Connection conn){
+		ArrayList<SuKien> listSK = new ArrayList<SuKien>();
+		String sql = "Select * from SuKien ORDER BY NameSK";
+		try {
+			Statement sta = conn.createStatement();
+			ResultSet result = sta.executeQuery(sql);
+			while(result.next()) {
+				SuKien sk = new SuKien();
+				sk.setIdSK(result.getString(1));
+				sk.setNameSK(result.getString(2));
+				sk.setTime(result.getTime(3));
+				sk.setNgaySK(result.getDate(4));
+				listSK.add(sk);
+			}
+			result.close();
+			sta.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		return listSK;
+	}
+}
