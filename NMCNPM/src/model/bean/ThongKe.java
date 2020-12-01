fd9633ec35c42d9ab97a46f22daddcc77a9fcdba
Chauptm <71380488+Chauptm@users.noreply.github.com>
2020-12-01 09:48:01 +0700

    update thongke

--- NMCNPM/src/model/bean/ThongKe.java
+++ NMCNPM/src/model/bean/ThongKe.java
@@ -2,4 +2,22 @@ package model.bean;
 
 public class ThongKe extends SoHoKhau {
 
+	private int solan;
+
+	public ThongKe(String idSHK, String chuHo, String diaChi, String eMail, int solan) {
+		super(idSHK, chuHo, diaChi, eMail);
+		this.solan = solan;
+	}
+
+	public ThongKe() {
+		super();
+	}
+
+	public int getSolan() {
+		return solan;
+	}
+
+	public void setSolan(int solan) {
+		this.solan = solan;
+	}
 }
