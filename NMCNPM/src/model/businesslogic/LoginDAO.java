750029a4ea0358c0af1f1d7c686ded812d2b646b
trungtinhpltn <63650257+trungtinhpltn@users.noreply.github.com>
2020-12-01 09:18:42 +0700

    Update

--- NMCNPM/src/model/businesslogic/LoginDAO.java
+++ NMCNPM/src/model/businesslogic/LoginDAO.java
@@ -1,1 +1,37 @@
+package model.businesslogic;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
+import model.bean.Login;
+
+public class LoginDAO {
+	public Login ktTonTai(Connection conn,String user, String pass) {
+		String sql="Select * from Login where (UserName=? and PassWord=?)";
+		PreparedStatement pst;
+		try {
+			pst= conn.prepareStatement(sql);
+			pst.setString(1, user);
+			pst.setString(2, pass);
+			ResultSet rs= pst.executeQuery();
+			if (rs.next()) {
+				Login login= new Login();
+				login.setUser(user);
+				return login;
+			}
+			rs.close();
+			pst.close();
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		return null;
+	}
+	public boolean chuyenTrang(String user) {
+		if((user.equalsIgnoreCase("cbToTruong")) || (user.equalsIgnoreCase("cbToPho")) || (user.equalsIgnoreCase("cbHanhChinh"))) {
+			return true;
+		}
+		return false;
+	}
+}
--- NMCNPM/src/view/LoginPage.java
+++ NMCNPM/src/view/LoginPage.java
@@ -1,42 +1,67 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import model.bean.Login;
+import model.businesslogic.LoginDAO;
+import model.db.DBConnection;
+
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.sql.Connection;
+import java.awt.event.ActionEvent;
+
 public class LoginPage extends JFrame {
 
-	private JPanel contentPane;
-
 	/**
-	 * Launch the application.
+	 * 
 	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtUser;
+	private JPasswordField txtPass;
+	private Connection conn= DBConnection.getConnection();
+	private JButton btnLogin = new JButton("Đăng nhập");
+	private LoginDAO lgD = new LoginDAO();
+	PressKey press;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LoginPage frame = new LoginPage();
-					frame.setVisible(true);
+					new LoginPage();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	private class PressKey implements KeyListener{
 
-	/**
-	 * Create the frame.
-	 */
-	public LoginPage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		public void keyTyped(KeyEvent e) {
+		
+		}
+
+		public void keyPressed(KeyEvent e) {
+			if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+				btnLogin.doClick();
+			}
+		}
+
+		public void keyReleased(KeyEvent e) {
+			
+		}	
 	}
-
 }
