44ec44cc5408625ff3fe727f199966095a396377
trungtinhpltn <63650257+trungtinhpltn@users.noreply.github.com>
2020-12-02 17:14:29 +0700

    Update by Tinhdz

--- NMCNPM/src/view/InformationAdmin.java
+++ NMCNPM/src/view/InformationAdmin.java
@@ -1,10 +1,21 @@
 package view;
 
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+
 import javax.swing.JPanel;
 
 public class InformationAdmin extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-	public InformationAdmin() {
+	/**
+	 * Create the panel.
+	 */
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	public InformationAdmin(Connection conn,String user) {
 
 	}
 
