1b53839e2d852766148163aa057eceddd81a7513
trungtinhpltn <63650257+trungtinhpltn@users.noreply.github.com>
2020-12-11 10:53:54 +0700

    Tinhdz

--- NMCNPM/src/model/businesslogic/AdminDAO.java
+++ NMCNPM/src/model/businesslogic/AdminDAO.java
@@ -5,11 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
